740a04d9728bdc40a473324fb1022506c2b7ef3e
James Bradley <hintonbradley@gmail.com>
2019-07-09 18:01:10 -0700

    added loaders and clean-webpack

--- src/index.js
+++ src/index.js
@@ -4,5 +4,3 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService();
-run(alertService, componentService);
-console.log('heello')
