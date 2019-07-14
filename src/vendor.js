45c72477197a3a8a60368cc318dd54e1e7f568ef
James Bradley <hintonbradley@gmail.com>
2019-07-14 16:04:52 -0700

    small changes

---
+++ src/vendor.js
@@ -0,0 +1,4 @@
+import "./main.scss";
+import 'bootstrap';
+
+console.log('Hi from vendor.js file')
--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,20 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // mode - default is production. if you want to run in development, you must add this key value (mode: development)
 // entry - an entrypoint for webpack to access your application
 // output - the file where the code should be outputted after webpack has completed compiling
 // path - node module allowing the application to create a file in a specific path wherever it's hosted.
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js"
+    },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ["style-loader","css-loader"]
             },
+            
             {
                 test: /\html$/,
                 use: ["html-loader"]
